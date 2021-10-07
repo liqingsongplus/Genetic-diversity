f781a31d4df7132d0a1430734728efabcecddf8e
liqingsong <329844894@qq.com>
2021-10-08 00:51:06 +0800

    Initial commit

---
+++ R/diveristy.R
@@ -0,0 +1,53 @@
+
+#' Genetic diversity
+#'
+#' You can use "diveristy()" to compute the genetic diversity of SNPs.
+#' @param data A genotype frame of SNPs in HMP formate
+#'
+#' @return A data frame about genetic diversity that can discribe the SNPs.
+#' @export
+#'
+#' @examples data("E5b_hmp")
+#' diveristy(E5b_hmp)
+
+diveristy <- function(data) {
+  A1 <- data[, -c(1:11)]
+  B <- NULL
+  out <- NULL
+  for (n in 1:nrow(A1)) {
+    AA1 <- table(t(A1[n, ]))
+    AA <- AA1[rownames(AA1) != "NN"]
+    CC <- rbind(number = AA, frequency = AA / sum(AA))
+    CC1 <- cbind.data.frame(Genotype = colnames(CC),
+                            frequency = round(CC[2, ], 4),
+                            number = CC[1, ])
+    B <- rbind(B, CC1)
+    for (i in 1:nrow(AA)) {
+      if (strsplit(rownames(AA), "")[[i]][1] !=
+          strsplit(rownames(AA), "")[[i]][2]) {
+        C <- sum(AA[[i]])
+      } else {
+        D1 <- max(AA[[i]])
+      }
+    }
+    D2 <- (D1 + C / 2) / sum(AA)
+    D3 <- 1 - D2
+    MAF <- min(D2, D3)
+    Ho <- C / sum(AA)
+    He <- MAF * (1 - MAF) * 2
+    PIC <- 1 - MAF^2 - (1 - MAF)^2 - 2 * (MAF^2) * ((1 - MAF)^2)
+    out <- rbind.data.frame(out, c(data[n, 1], data[n, 4], data[n, 2],
+                                   round(Ho, 4), round(He, 4), round(PIC, 4),
+                                   round(MAF, 4)))
+    if (nrow(AA) == 2) {
+      out <- rbind(out, NA)
+    } else if (nrow(AA) == 3) {
+      out <- rbind(out, NA, NA)
+    }
+  }
+  colnames(out) <- c("dbSNP", "Position", "Ref/Alt", "Ho", "He", "PIC", "MAF")
+  colnames(B) <- c("Genotype", "Freq_of_geno", "number")
+  BB <- cbind(out, B)
+  rownames(BB) <- NULL
+  return(BB)
+}
---
+++ R/ld_plot.R
@@ -0,0 +1,36 @@
+#' LD block heatmap
+#'
+#' ld_plot() is used to produce a graphical display, as a heat map, of pairwise
+#'  linkage disequilibrium (LD) measurements for SNPs.
+#' @param data A genotype frame of SNPs in HMP format
+#' @param pos The position of snps on chromosomes
+#'
+#' @return The file named LDheatmap.pdf will be generated in the current
+#' directory.
+#' @export
+#'
+#' @examples
+#' library(genetic)  # 加载包
+#' data("E5b_hmp") #读取数据
+#' ld_plot(E5b_hmp, E5b_hmp$pos) # 在当前文件夹下生成LDheatmap.pdf
+
+ld_plot <- function(data, pos) {
+  row.names(data) <- pos
+  snpdata <- as.data.frame(t(data[, -c(1:11)]))
+  for (i in seq_len(ncol(snpdata))) {
+    for (j in seq_len(nrow(snpdata))) {
+      snpdata[j, i] <- gsub("([A-Z])([A-Z])", "\\1/\\2", snpdata[j, i])
+      snpdata[j, i] <- gsub("N/N", "", snpdata[j, i])
+    }
+  }
+  for (i in seq_len(ncol(snpdata))) {
+    snpdata[, i] <- genetics::as.genotype(snpdata[, i])
+  }
+  color.rgb <- colorRampPalette(rev(c("white", "red")), space = "rgb")
+  pdf("LDheatmap.pdf")
+  LDheatmap::LDheatmap(snpdata, as.numeric(pos),
+    color = color.rgb(10), flip =
+      TRUE, SNP.name = colnames(snpdata), LDmeasure = "D'"
+  )
+  dev.off()
+}
